4483773889a8d02864a5b8a78d0d2c0c2c55424d
David Raskin <32804095+davidraskin8@users.noreply.github.com>
2025-11-16 06:31:18 +0000

    Added merge

--- b-plus-tree-leaf.py
+++ b-plus-tree-leaf.py
@@ -1,4 +1,5 @@
 import bisect
+import math
 
 def insert(arr, data):
     bisect.insort(arr, data, key = lambda d: d.k)
@@ -14,10 +15,12 @@ class Data:
         self.payload = payload
 
 class InternalNode:
-    def __init__(self, next=None, max_size = 0):
+    def __init__(self, order, next=None):
         self.arr = []
         self.next = next
-        self.max_size = max_size
+        self.max_size = order - 1
+        self.min_size = math.ceil(order / 2) - 1
+        self.order = order
 
     def insert(self, data):
         if len(self.arr) < self.max_size:
@@ -31,4 +34,12 @@ class InternalNode:
         else:
             insert(arr2, data)
 
+    def merge_next(self):
+        next = self.next
+        assert next is not None
+        assert len(next.arr) == next.min_size
+        assert len(self.arr) < next.min_size
+
+        self.arr += next.arr
+        self.next = next.next
 
